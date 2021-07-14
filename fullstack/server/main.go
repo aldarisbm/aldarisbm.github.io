f0473d65e058d5a16373395cc16365c1c209f726
Jose Berrio <aldarisbm@gmail.com>
2021-07-13 22:06:28 -0400

    basic health endpoint, starting the backend

--- fullstack/server/main.go
+++ fullstack/server/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/aldarisbm/bachataistakingover/structs"
 	"github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Test Test Test")
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	p := structs.Health{
+		Message:    "OK",
+		StatusCode: 200,
+	}
+
+	json.NewEncoder(w).Encode(p)
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.Handle("/", staticController.Home).Methods("GET")
-	r.Handle("/contact", staticController.Contact).Methods("GET")
-	r.Handle("/faq", staticController.Faq).Methods("GET")
-	r.HandleFunc("/signup", usersController.New).Methods("GET")
-	r.HandleFunc("/signup", usersController.Create).Methods("POST")
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/healthz", health).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ fullstack/server/structs/health.go
@@ -0,0 +1,6 @@
+package structs
+
+type Health struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"statusCode"`
+}
